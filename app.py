daf4934f0f52196b25940cae42682aad40ddc67e
OneChainTech <44761375+OneChainTech@users.noreply.github.com>
2023-08-28 21:43:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ if 'chat_history' not in st.session_state:
 
 # 标题图
 # st.title('AiDoctor')
-st.set_page_config(page_title='iDoctor 人工智能医疗助理')
+st.set_page_config(page_title='iDoctor 人工智能医疗信息服务助理')
 
 st.image("deer.png", use_column_width=True)
     
 # 输入问题聊天框
-user_input = st.text_area("\n\n", placeholder='请输入您想了解的医疗问题')
+user_input = st.text_area("输入您想了解的医疗问题", placeholder='')
 # text = st.text_area('请输入文本', height=3)
 
 # checkbox_state = st.checkbox("清除历史数据")
